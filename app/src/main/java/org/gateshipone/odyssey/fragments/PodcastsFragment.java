234bb714486ee54a89b72b7994dc491d15662708
Frederik Lütkes <frederik.luetkes@googlemail.com>
2021-09-03 22:12:45 +0200

    fifth part lets add a context menu

--- app/src/main/java/org/gateshipone/odyssey/fragments/PodcastsFragment.java
+++ app/src/main/java/org/gateshipone/odyssey/fragments/PodcastsFragment.java
@@ -22,10 +22,13 @@
 
 package org.gateshipone.odyssey.fragments;
 
-import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.RemoteException;
+import android.view.ContextMenu;
 import android.view.LayoutInflater;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
@@ -34,17 +37,20 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.preference.PreferenceManager;
 
 import org.gateshipone.odyssey.R;
 import org.gateshipone.odyssey.activities.GenericActivity;
 import org.gateshipone.odyssey.adapter.TracksAdapter;
 import org.gateshipone.odyssey.models.TrackModel;
+import org.gateshipone.odyssey.utils.PreferenceHelper;
 import org.gateshipone.odyssey.utils.ThemeUtils;
 import org.gateshipone.odyssey.viewmodels.GenericViewModel;
 import org.gateshipone.odyssey.viewmodels.PodcastsViewModel;
-import org.gateshipone.odyssey.viewmodels.TrackViewModel;
 
 public class PodcastsFragment extends OdysseyFragment<TrackModel> implements AdapterView.OnItemClickListener {
+
+    private PreferenceHelper.LIBRARY_TRACK_CLICK_ACTION mClickAction;
 
     public static PodcastsFragment newInstance() {
         return new PodcastsFragment();
@@ -80,9 +86,12 @@ public class PodcastsFragment extends OdysseyFragment<TrackModel> implements Ada
         mEmptyView = view.findViewById(R.id.empty_view);
 
         // set empty view message
-        ((TextView) view.findViewById(R.id.empty_view_message)).setText("no podcasts found");
+        ((TextView) view.findViewById(R.id.empty_view_message)).setText(R.string.empty_podcasts_message);
 
-//        registerForContextMenu(mListView);
+        registerForContextMenu(mListView);
+
+        final SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getContext());
+        mClickAction = PreferenceHelper.getClickAction(sharedPreferences, getContext());
 
         // setup observer for the live data
         getViewModel().getData().observe(getViewLifecycleOwner(), this::onDataReady);
@@ -99,7 +108,7 @@ public class PodcastsFragment extends OdysseyFragment<TrackModel> implements Ada
         if (mToolbarAndFABCallback != null) {
             // set toolbar behaviour and title
             // TODO use resource for title
-            mToolbarAndFABCallback.setupToolbar("Podcasts", false, true, false);
+            mToolbarAndFABCallback.setupToolbar(getString(R.string.fragment_title_podcasts), false, true, false);
         }
     }
 
@@ -110,9 +119,53 @@ public class PodcastsFragment extends OdysseyFragment<TrackModel> implements Ada
 
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        // TODO adjust to click action from settings
-        playPodcast(position, true);
+        switch (mClickAction) {
+            case ACTION_ADD_SONG:
+                enqueuePodcast(position, false);
+                break;
+            case ACTION_PLAY_SONG:
+                playPodcast(position, false);
+                break;
+            case ACTION_PLAY_SONG_NEXT:
+                enqueuePodcast(position, true);
+                break;
+            case ACTION_CLEAR_AND_PLAY:
+                playPodcast(position, true);
+                break;
+        }
     }
+
+    @Override
+    public void onCreateContextMenu(@NonNull ContextMenu menu, @NonNull View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        MenuInflater inflater = getActivity().getMenuInflater();
+        inflater.inflate(R.menu.context_menu_podcasts_fragment, menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+
+        if (info == null) {
+            return super.onContextItemSelected(item);
+        }
+
+        final int itemId = item.getItemId();
+
+        if (itemId == R.id.fragment_podcasts_action_enqueue) {
+            enqueuePodcast(info.position, false);
+            return true;
+        } else if (itemId == R.id.fragment_podcasts_action_enqueueasnext) {
+            enqueuePodcast(info.position, true);
+            return true;
+        } else if (itemId == R.id.fragment_podcasts_action_play) {
+            playPodcast(info.position, false);
+            return true;
+        }
+
+        return super.onContextItemSelected(item);
+    }
+
 
     private void playPodcast(final int position, final boolean clearPlaylist) {
         final TrackModel podcast = mAdapter.getItem(position);
